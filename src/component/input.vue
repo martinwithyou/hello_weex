624474a489f60973801cc29bc31e2f56be23aa6a
martinwithyou <406410589@qq.com>
2018-02-06 10:43:47 +0800

    this is my first weex demo at android system

---
+++ src/component/input.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="topheader">
+      <text class="top_text"> 技术胖技术胖技术胖技术胖技术胖技术胖技术胖技术胖技术胖技术胖技术胖技术胖</text>
+  </div>
+</template>
+ 
+<script>
+export default {
+  
+}
+</script>
+ 
+ 
+<style scoped>
+    .topheader{
+        background-color:red;
+        padding:10px;
+    }
+    .top_text{
+        color:#fff;
+        text-align: center;
+        font-size:50px;
+        lines:1;
+    }
+</style>
---
+++ src/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="wrapper" @click="update">
+  	<image class="testImage" src="http://7xjyw1.com1.z0.glb.clouddn.com/20170412101827.png"></image>
+    <image :src="logoUrl" class="logo"></image>
+    <text class="title">Hello {{target}}</text>
+    <div><input class="testInput" type="text" placeholder="瞎输入点什么"  /></div>
+    <text class="desc">Now, let's use vue to build your weex app.</text>
+    <head/>
+    <video/>
+  </div>
+</template>
+
+<style>
+	.testImage{
+    width:400px;
+    height:300px;
+  }
+  .wrapper { align-items: center; margin-top: 0px; }
+  .title { padding-top:40px; padding-bottom: 40px; font-size: 48px; }
+  .logo { width: 360px; height: 156px; }
+  .desc { padding-top: 20px; color:#888; font-size: 24px;}
+</style>
+
+<script>
+	import head from './component/head.vue'
+	import video from './component/video.vue'
+	var modal = weex.requireModule('modal')
+  export default {
+    data: {
+      logoUrl: 'http://img1.vued.vanthink.cn/vued08aa73a9ab65dcbd360ec54659ada97c.png',
+      target: '对方水电宣传宣传费水电费噢噢噢噢'
+    },
+    created(){
+      console.log('will show toast')
+        modal.toast({
+          message: 'ddddddddddd初始化成功。',
+          duration:5
+        })
+    },
+    components:{
+    	head,
+    	video
+    },
+    methods: {
+      update: function (e) {
+        this.target = 'Weex'
+        console.log('target:', this.target)
+      }
+    }
+  }
+</script>
